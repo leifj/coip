d94de32acf89d30c031596b3655f6e8005028919
Leif Johansson <leifj@sunet.se>
2010-08-04 15:00:31 +0200

    make one profile 'primary'

--- coip/apps/userprofile/models.py
+++ coip/apps/userprofile/models.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.ForeignKey(User,blank=True,null=True,related_name='profiles')
     display_name = models.CharField(max_length=255,blank=True,null=True)
+    primary = models.BooleanField()
     email = models.EmailField(blank=True,null=True)
     idp = models.CharField(max_length=255,blank=True,null=True)
     identifier = models.CharField(max_length=1023,unique=True)
@@ -18,9 +19,17 @@ class UserProfile(models.Model):
     def __unicode__(self):
         return "%s [%s] - %s" % (self.identifier,self.user.username,self.display_name)
 
+    def make_primary(self):
+        for p in UserProfile.objects.filter(user=self.user).all:
+            p.primary = False
+        self.primary = True
 
 def last_used_profile(user):
     return UserProfile.objects.filter(user=user).order_by('lastupdated')[0]
+
+def primary_profile(user):
+    return UserProfile.objects.filter(user=user,primary=True)[0]
+
     
 class PKey(models.Model):
     user_profile = models.ForeignKey(UserProfile,related_name='keys')
