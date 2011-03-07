7777ab12df9e91bb657f000bf58e6737e89b51a3
Leif Johansson <leifj@sunet.se>
2011-03-07 23:56:05 +0100

    import cleanup

--- coip/apps/tag/views.py
+++ coip/apps/tag/views.py
@@ -3,16 +3,15 @@ Created on Mar 3, 2011
 
 @author: jbn
 '''
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseRedirect, Http404
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from coip.multiresponse import respond_to, json_response, render403
+from coip.multiresponse import respond_to, render403
 import logging
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
 from coip.apps.membership.models import Membership
 from tagging.models import Tag
 
+@login_required
 def modify(request, type, id):
     if type == "membership":
         tagobj = get_object_or_404(Membership, pk=id)
