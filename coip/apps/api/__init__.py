f1a658408cf57d65ef3d18fe1e62e9cb59011331
Leif Johansson <leifj@sunet.se>
2011-11-08 19:31:15 +0100

    removed more of tastypie

--- coip/apps/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from tastypie.api import Api
-from coip.apps.api.resources import UserResource, NameResource
-
-v1_api = Api(api_name="1.0")
-v1_api.register(UserResource())
-v1_api.register(NameResource())
