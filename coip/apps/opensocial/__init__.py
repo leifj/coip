9ba73ae91642d07aa78789e6e0cedf6ed6250300
Leif Johansson <leifj@sunet.se>
2011-11-08 10:20:23 +0100

    cleanup

--- coip/apps/opensocial/__init__.py
+++ coip/apps/opensocial/__init__.py
@@ -1,6 +1,5 @@
 from tastypie.api import Api
-from coip.apps.opensocial.people import PersonResource, MembershipResource,\
-    GroupResource
+from coip.apps.opensocial.people import PersonResource, MembershipResource, GroupResource
 
 opensocial_v1 = Api(api_name = "1.0")
 opensocial_v1.register(PersonResource())
